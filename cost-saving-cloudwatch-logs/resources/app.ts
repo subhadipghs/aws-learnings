905f662eab8af3f61b111f69a3266b3f4398dc83
Subhadip  Ghosh <subhadipghosh@antstack.io>
2022-09-15 13:33:25 +0530

    linter: remove semicolon

--- cost-saving-cloudwatch-logs/resources/app.ts
+++ cost-saving-cloudwatch-logs/resources/app.ts
@@ -1,4 +1,4 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 
 /**
  *
@@ -13,23 +13,23 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 export const lambdaHandler = async (
   event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> => {
-  let response: APIGatewayProxyResult;
+  let response: APIGatewayProxyResult
   try {
     response = {
       statusCode: 200,
       body: JSON.stringify({
         message: 'hello world',
       }),
-    };
+    }
   } catch (err) {
-    console.log(err);
+    console.log(err)
     response = {
       statusCode: 500,
       body: JSON.stringify({
         message: 'some error happened',
       }),
-    };
+    }
   }
 
-  return response;
-};
+  return response
+}
